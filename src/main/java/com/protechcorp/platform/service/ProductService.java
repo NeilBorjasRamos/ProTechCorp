52ed4c0635191b53e68de16d06d870ae32f54e73
Angelo98 <u201623628@upc.edu.pe>
2019-10-23 21:14:18 -0500

    Service Int

---
+++ src/main/java/com/protechcorp/platform/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.protechcorp.platform.service;
+
+import com.protechcorp.platform.model.Product;
+
+public interface ProductService {
+	
+	public abstract int createProduct(Product product);
+
+}
