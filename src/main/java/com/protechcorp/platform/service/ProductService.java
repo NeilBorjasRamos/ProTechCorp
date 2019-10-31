bdcaa1a30b27be260c2c6709c79c6fe590cf422d
Eduardo <eduardo_estela_1999@outlook.com>
2019-10-30 20:22:14 -0500

    Ver 0.0

--- src/main/java/com/protechcorp/platform/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.protechcorp.platform.service;
-
-import com.protechcorp.platform.model.Product;
-
-public interface ProductService {
-	
-	public abstract int createProduct(Product product);
-
-}
